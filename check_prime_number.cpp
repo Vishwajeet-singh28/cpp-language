9a62408fe58141713143af1f04bcdb95f753c581
VISHWAJEET SINGH <vishwajeetsinghrajput75@gmail.com>
2026-07-29 21:59:56 +0530

    Simplify prime number output logic
    Refactor prime number check to only output result once.

--- check_prime_number.cpp
+++ check_prime_number.cpp
@@ -4,16 +4,21 @@ using namespace std;
 
 
 int prime(int n){
+
+     if (n <= 1)
+    {
+        cout << n << " is not a prime number";
+        return 0;
+    }
     for(int i=2;i<n;i++){
         if(n%i==0){
         cout<<n<<" is not prime number "<<endl;
+        return 0;
         }
-        else{
-            cout<<n<<" is prime number "<<endl;
-        }
+    }
+    cout<<n<<" is prime number "<<endl;
     return 0;
     }
-}
 int main(){
     int n;
     cout<<"enter number: "<<endl;
